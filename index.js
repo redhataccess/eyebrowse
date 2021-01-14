d5addc31ede7eb4e2e4a8d5011657bce1c39bad8
mwcz <mwc@clayto.com>
2021-01-14 12:48:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { StaticApp } = require('@keystonejs/app-static');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'eyebrowse';
+const adapterConfig = { mongoUri: 'mongodb://localhost/eyebrowse' };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new StaticApp({ path: '/', src: 'public' }),
+    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true }),
+  ],
+};
