3dfecd6d847d94bb779f3326d8363ba3add5cd61
mwcz <mwc@clayto.com>
2021-02-04 15:48:40 -0500

    add better breadcrumbs

--- client/script.js
+++ client/script.js
@@ -2,6 +2,29 @@ import Vue from "vue/dist/vue.esm";
 import "@patternfly/pfe-icon";
 import "@patternfly/pfe-number";
 import "@patternfly/pfe-datetime";
+import "@patternfly/pfe-button";
+
+const cache = {
+  http: {},
+  files: {
+    // prepopulate a fake "root" path
+    "": {
+      dir: true,
+      name: "",
+      path: "",
+      size: 0,
+    }
+  },
+};
+
+function cacheGet(type, path) {
+  return cache[type][path];
+}
+
+function cacheStash(type, path, fileObj) {
+  cache[type][path] = fileObj;
+  console.log('cache updated', {cache});
+}
 
 async function fetchConfig() {
   if (!window.eyebrowse_config) {
@@ -11,10 +34,30 @@ async function fetchConfig() {
   return window.eyebrowse_config;
 }
 
-async function fetchData() {
+async function fetchFileList(path) {
+  const cached = cacheGet('http', path );
+  if (cached) {
+    return cached;
+  }
   const config = await fetchConfig();
-  const res = await fetch(config.api_url + "/files/");
-  return await res.json();
+  const res = await fetch(config.api_url + "/files/" + path);
+  const fileList = await res.json();
+  const allFiles = fileList.data.allFiles;
+
+  // update http cache
+  cacheStash('http', path, allFiles);
+
+  // update file object cache
+  allFiles.forEach(f => cacheStash('files', f.path, f));
+
+  // establish references to each file's parent dir
+  allFiles.forEach(f => {
+    f.parentRef = cacheGet("files", f.parent);
+  });
+
+  console.log({allFiles});
+
+  return allFiles;
 }
 
 const app = new Vue({
@@ -23,11 +66,45 @@ const app = new Vue({
     allFiles: {},
     appName: "Eyebrowse",
     currentPath: "",
+    currentDir: {},
+    breadcrumbs: [],
+  },
+  methods: {
+    openFile: function (event) {
+      console.log(`NOIMPL open file`);
+    },
+    browseDir: async function(path) {
+      this.currentPath = path;
+      const allFiles = await fetchFileList(this.currentPath);
+      console.table(allFiles);
+      this.allFiles = allFiles;
+
+      // update the current dir reference
+      this.currentDir = cacheGet('files', path);
+      console.log({currentDir: this.currentDir});
+
+      // update the array of breadcrumbs
+      this.breadcrumbs = [];
+      let next = this.currentDir;
+      while(next.parentRef) {
+        this.breadcrumbs.push(next);
+        next = next.parentRef;
+      }
+      this.breadcrumbs.reverse();
+
+      console.log('breadcrumbs', this.breadcrumbs);
+    },
+    // browseDir: async function (dir) {
+    //   console.log({ currentPathSplit: this.currentPathSplit});
+    // }
+  },
+  watch: {
+    // currentPath: function (val) {
+    //   console.log({currentPath: val});
+    // },
   },
   created: async function() {
-    const { data } = await fetchData();
-    console.table(data.allFiles);
-    this.allFiles = data.allFiles;
+    this.browseDir("");
   },
 });
 
